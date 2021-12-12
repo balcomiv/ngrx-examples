1ed2b9f6e0ecd2e44a2e5bbf884c2454b63a82bf
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-12-12 17:30:33 -0500

    add - counter component

--- libs/ngrx-io/counter/feature-main/src/lib/ngrx-io-counter-feature-main.module.ts
+++ libs/ngrx-io/counter/feature-main/src/lib/ngrx-io-counter-feature-main.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    CounterComponent
+  ],
 })
 export class NgrxIoCounterFeatureMainModule {}
