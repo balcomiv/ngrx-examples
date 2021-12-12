c923297d6bc94d855e6d6f0b7b3a37b565f6494c
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-12-12 17:45:26 -0500

    udpate - counter component

--- libs/ngrx-io/counter/feature-main/src/lib/counter/counter.component.ts
+++ libs/ngrx-io/counter/feature-main/src/lib/counter/counter.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { getCounterState } from '../../../../data-access/src';
+import * as CounterActions from '../../../../data-access/src/lib/+state/counter.actions';
 @Component({
   selector: 'ngrx-examples-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.scss']
+  styleUrls: ['./counter.component.scss'],
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent {
+  count$: Observable<number>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: Store) {
+    this.count$ = this.store.select(getCounterState);
   }
 
+  increment() {
+    this.store.dispatch(CounterActions.increment());
+  }
+
+  decrement() {
+    this.store.dispatch(CounterActions.decrement());
+  }
+
+  reset() {
+    this.store.dispatch(CounterActions.reset());
+  }
 }
