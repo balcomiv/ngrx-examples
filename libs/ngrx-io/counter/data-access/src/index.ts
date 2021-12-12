c18d115707bf08e0310db36dd81f84750eebbd6f
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-12-12 16:59:54 -0500

    add - counter ngrx feature state

--- libs/ngrx-io/counter/data-access/src/index.ts
+++ libs/ngrx-io/counter/data-access/src/index.ts
@@ -1,1 +1,5 @@
+export * from './lib/+state/counter.models';
+export * from './lib/+state/counter.selectors';
+export * from './lib/+state/counter.reducer';
+export * from './lib/+state/counter.actions';
 export * from './lib/ngrx-io-counter-data-access.module';
--- libs/ngrx-io/counter/data-access/src/lib/ngrx-io-counter-data-access.module.ts
+++ libs/ngrx-io/counter/data-access/src/lib/ngrx-io-counter-data-access.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import * as fromCounter from './+state/counter.reducer';
+import { CounterEffects } from './+state/counter.effects';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    StoreModule.forFeature(
+      fromCounter.COUNTER_FEATURE_KEY,
+      fromCounter.reducer
+    ),
+    EffectsModule.forFeature([CounterEffects]),
+  ],
 })
 export class NgrxIoCounterDataAccessModule {}
